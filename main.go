2d6b5cd32acbffe3de60248cb781a1cd4ec86bf5
Ryan <ryan.maceachern@gmail.com>
2022-08-17 00:24:10 -0700

    declare basic variables

--- main.go
+++ main.go
@@ -7,3 +7,15 @@ import (
 func main() {
 	fmt.Println("Hello World!")
 }
+
+// simple var types, declared this way take on null values
+// "" for string, false for bool, 0 for numbers
+var myBoolean bool
+var myString string
+
+// declare two vars in one line of the same type
+var myInt, otherInt int
+
+// declare a var with a value
+var myHelloString = "Hello"
+
